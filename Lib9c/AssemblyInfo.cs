31a2a598ffbc3c12ee7c4599603668131eb370aa
s2quake <han0210@netsgo.com>
2024-05-15 07:54:38 +0900

    chore: Use the libplanet solution path variable in msbuild process.

--- Lib9c/AssemblyInfo.cs
+++ Lib9c/AssemblyInfo.cs
@@ -1,3 +1,5 @@
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Lib9c.Tests")]
+[assembly: InternalsVisibleTo("LibplanetConsole.Banks")]
+[assembly: InternalsVisibleTo("LibplanetConsole.Stakings")]
